c5365ff886e50e351b829f9999f2394e3d0076d3
Doug Lin <douglas.lin@gmail.com>
2021-03-12 13:30:34 -0500

    Update setup_sat_control_hosts.sh

--- setup_sat_control_hosts.sh
+++ setup_sat_control_hosts.sh
@@ -1,1 +1,1 @@
-ansible-playbook -vvv --ssh-common-args='-o StrictHostKeyChecking=no' --private-key=/Users/douglin/.ssh/bluemix satellite_playbook.yml --extra-vars "satellite-group" -i satellite_control_hosts
+ansible-playbook -vvv --ssh-common-args='-o StrictHostKeyChecking=no' --private-key=<your SSH key> satellite_playbook.yml --extra-vars "satellite-group" -i satellite_control_hosts
